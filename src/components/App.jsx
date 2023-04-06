c576ac9f5c20d60e61e3e3cfdd4a014c77f6fe58
Anderson Peñaloza <designeromega@gmail.com>
2023-04-06 16:54:14 +0200

    update App component to use layout and login

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import '../styles/global.scss';
+import Layout from '../containers/Layout';
+import Login from '../containers/Login';
 
 const App = () => {
-  return <div>Hola mundo</div>;
+  return (
+    <Layout>
+      <Login />
+    </Layout>
+  );
 };
 
 export default App;
