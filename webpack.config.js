d6091b74ee347343bc6ff92fc4640faf687bcbd1
Anderson Peñaloza <designeromega@gmail.com>
2023-04-07 14:43:03 +0200

    add configuration to handle multiple routes

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
+    publicPath: '/',
   },
   mode: 'development',
   resolve: {
@@ -46,5 +47,6 @@ module.exports = {
     allowedHosts: path.join(__dirname, 'dist'),
     compress: true,
     port: 3005,
+    historyApiFallback: true,
   },
 };
