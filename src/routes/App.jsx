6694ee7320360b1a13c20c1d67b568d4242d9b9f
Anderson Peñaloza <designeromega@gmail.com>
2023-04-07 14:44:06 +0200

    add base layout to start building routes

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,12 +1,30 @@
 import React from 'react';
+import {
+  BrowserRouter,
+  Route,
+  Switch,
+} from 'react-router-dom';
+
 import Layout from '../containers/Layout';
 import Login from '../containers/Login';
+import RecoveryPassword from '../containers/RecoveryPassword';
 
 const App = () => {
   return (
-    <Layout>
-      <Login />
-    </Layout>
+    <BrowserRouter>
+      <Layout>
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <Route exact path="/login" component={Login} />
+          <Route
+            exact
+            path="/recovery"
+            component={RecoveryPassword}
+          />
+          <Route path="*" component={Login} />
+        </Switch>
+      </Layout>
+    </BrowserRouter>
   );
 };
 
